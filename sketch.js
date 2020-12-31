db65848d7707ea8bd59e730d3d728bf0400fe52d
Ysaraff <70392768+Ysaraff@users.noreply.github.com>
2020-12-30 19:46:50 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var ball;
+var position, database;
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database();
+    var nodeLocation = database.ref("Ball/positions");
+    nodeLocation.on("value", readOp, showError)
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+database.ref("Ball/positions").update({
+    x: ball.x + x,
+    y: ball.y + y
+});
+}
+function readOp(data){
+position = data.val();
+ball.x = position.x;
+ball.y = position.y;
+}
+function showError(){
+    console.log(error);
+}
